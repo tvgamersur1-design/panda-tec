92af4c8f993365d350d9b4b90dcdc6cf60dfb79c
tvgamersur2-Pro <tvgamersur2@gmail.com>
2026-05-03 15:33:43 -0500

    fix: API_URL dinamico para produccion

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:3000/api'
+    : `${window.location.origin}/api`;
 
 // Fecha actual
 function actualizarFecha() {
